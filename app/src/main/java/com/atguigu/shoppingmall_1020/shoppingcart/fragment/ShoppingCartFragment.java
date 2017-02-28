81e9a2c81dca333e73981b635c376e7ff755476f
atguiguafu <541433511@qq.com>
2017-02-28 10:43:16 +0800

    购物车展示页面

--- app/src/main/java/com/atguigu/shoppingmall_1020/shoppingcart/fragment/ShoppingCartFragment.java
+++ app/src/main/java/com/atguigu/shoppingmall_1020/shoppingcart/fragment/ShoppingCartFragment.java
@@ -1,14 +1,22 @@
 package com.atguigu.shoppingmall_1020.shoppingcart.fragment;
 
-import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.atguigu.shoppingmall_1020.R;
 import com.atguigu.shoppingmall_1020.base.BaseFragment;
-import com.atguigu.shoppingmall_1020.home.bean.GoodsBean;
 import com.atguigu.shoppingmall_1020.shoppingcart.utils.CartStorage;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * 作者：尚硅谷-杨光福 on 2017/2/22 11:36
@@ -17,14 +25,38 @@ import com.atguigu.shoppingmall_1020.shoppingcart.utils.CartStorage;
  * 作用：购物车Fragment
  */
 public class ShoppingCartFragment extends BaseFragment {
-    private TextView textView;
+    @InjectView(R.id.tv_shopcart_edit)
+    TextView tvShopcartEdit;
+    @InjectView(R.id.recyclerview)
+    RecyclerView recyclerview;
+    @InjectView(R.id.checkbox_all)
+    CheckBox checkboxAll;
+    @InjectView(R.id.tv_shopcart_total)
+    TextView tvShopcartTotal;
+    @InjectView(R.id.btn_check_out)
+    Button btnCheckOut;
+    @InjectView(R.id.ll_check_all)
+    LinearLayout llCheckAll;
+    @InjectView(R.id.checkbox_delete_all)
+    CheckBox checkboxDeleteAll;
+    @InjectView(R.id.btn_delete)
+    Button btnDelete;
+    @InjectView(R.id.btn_collection)
+    Button btnCollection;
+    @InjectView(R.id.ll_delete)
+    LinearLayout llDelete;
+    @InjectView(R.id.iv_empty)
+    ImageView ivEmpty;
+    @InjectView(R.id.tv_empty_cart_tobuy)
+    TextView tvEmptyCartTobuy;
+    @InjectView(R.id.ll_empty_shopcart)
+    LinearLayout llEmptyShopcart;
+
     @Override
     public View initView() {
-        textView = new TextView(mContext);
-        textView.setTextSize(20);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_shopping_cart, null);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     /**
@@ -36,17 +68,7 @@ public class ShoppingCartFragment extends BaseFragment {
         super.initData();
 
 
-        Log.e("TAG","购物车的数据被初始化了...");
-        textView.setText("购物车内容");
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GoodsBean goodsBean  = new GoodsBean();
-                goodsBean.setProduct_id("10659");
-                goodsBean.setNumber(3);
-                CartStorage.getInstance(mContext).updateData(goodsBean);
-            }
-        });
+        Log.e("TAG", "购物车的数据被初始化了...");
     }
 
     @Override
@@ -58,8 +80,46 @@ public class ShoppingCartFragment extends BaseFragment {
     @Override
     public void onHiddenChanged(boolean hidden) {
         super.onHiddenChanged(hidden);
-        for (int i=0;i<CartStorage.getInstance(mContext).getAllData().size();i++){
-            Log.e("TAG",""+CartStorage.getInstance(mContext).getAllData().get(i).toString());
+        if (!hidden) {
+            for (int i = 0; i < CartStorage.getInstance(mContext).getAllData().size(); i++) {
+                Log.e("TAG", "" + CartStorage.getInstance(mContext).getAllData().get(i).toString());
+            }
+        }
+
+    }
+
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
+    }
+
+    @OnClick({R.id.tv_shopcart_edit, R.id.checkbox_all, R.id.btn_check_out, R.id.checkbox_delete_all, R.id.btn_delete, R.id.btn_collection, R.id.tv_empty_cart_tobuy})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_shopcart_edit:
+                Toast.makeText(mContext, "编辑", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.checkbox_all:
+                Toast.makeText(mContext, "全选", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_check_out:
+                Toast.makeText(mContext, "结算", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.checkbox_delete_all:
+                Toast.makeText(mContext, "删除全选", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_delete:
+                Toast.makeText(mContext, "删除按钮", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_collection:
+                Toast.makeText(mContext, "收藏", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_empty_cart_tobuy:
+                Toast.makeText(mContext, "去逛逛", Toast.LENGTH_SHORT).show();
+                break;
         }
     }
 }
