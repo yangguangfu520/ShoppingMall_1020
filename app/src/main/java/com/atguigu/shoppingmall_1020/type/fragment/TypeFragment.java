2545196cbbbb749248f3d3c434e92ce94e7e9993
atguiguafu <541433511@qq.com>
2017-03-03 09:58:32 +0800

    创建ListFragment和TagFragemnt并且切换

--- app/src/main/java/com/atguigu/shoppingmall_1020/type/fragment/TypeFragment.java
+++ app/src/main/java/com/atguigu/shoppingmall_1020/type/fragment/TypeFragment.java
@@ -1,15 +1,19 @@
 package com.atguigu.shoppingmall_1020.type.fragment;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
 
+import com.atguigu.shoppingmall_1020.MainActivity;
 import com.atguigu.shoppingmall_1020.R;
 import com.atguigu.shoppingmall_1020.base.BaseFragment;
 import com.flyco.tablayout.SegmentTabLayout;
 import com.flyco.tablayout.listener.OnTabSelectListener;
+
+import java.util.ArrayList;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -30,6 +34,12 @@ public class TypeFragment extends BaseFragment {
     FrameLayout flType;
     private String[] titles = {"分类","标签"};
 
+    private ArrayList<BaseFragment> fragments;
+    /**
+     * 刚才被显示的fragment
+     */
+    private Fragment tempFragment;
+
     @Override
     public View initView() {
         View view = View.inflate(mContext, R.layout.fragment_type, null);
@@ -45,11 +55,18 @@ public class TypeFragment extends BaseFragment {
     public void initData() {
         super.initData();
         Log.e("TAG", "分类的数据被初始化了...");
+        initListener();
+        initFragment();
+        switchFragment(fragments.get(0));
+    }
+
+    private void initListener() {
         tl1.setTabData(titles);
         tl1.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelect(int position) {
-                Toast.makeText(mContext, "position=="+position, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(mContext, "position=="+position, Toast.LENGTH_SHORT).show();
+                switchFragment(fragments.get(position));
             }
 
             @Override
@@ -57,6 +74,53 @@ public class TypeFragment extends BaseFragment {
 
             }
         });
+    }
+
+    /**
+     * 初始化Fragment
+     */
+    private void initFragment() {
+        fragments = new ArrayList<>();
+        fragments.add(new ListFragment());
+        fragments.add(new TagFragment());
+    }
+
+    private void switchFragment(Fragment currentFragment) {
+        //切换的不是同一个页面
+        if(tempFragment != currentFragment){
+
+            MainActivity activity = (MainActivity) mContext;
+            //得到FragmentMager
+            FragmentTransaction ft = activity.getSupportFragmentManager().beginTransaction();
+            //如果没有添加就添加
+            if(!currentFragment.isAdded()){
+                //缓存的隐藏
+                if(tempFragment != null){
+                    ft.hide(tempFragment);
+                }
+
+                //添加
+                ft.add(R.id.fl_type,currentFragment);
+
+            }else{
+                //缓存的隐藏
+                if(tempFragment != null){
+                    ft.hide(tempFragment);
+                }
+
+                //显示
+                ft.show(currentFragment);
+            }
+
+            //事务提交
+            ft.commit();
+
+
+            //把当前的赋值成缓存的
+            tempFragment = currentFragment;
+
+        }
+
     }
 
 
