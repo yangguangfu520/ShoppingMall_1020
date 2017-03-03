4c813feaa3246162d7e99162cb4a16174f1872d6
atguiguafu <541433511@qq.com>
2017-03-03 09:47:41 +0800

    分类页面布局

--- app/src/main/java/com/atguigu/shoppingmall_1020/type/fragment/TypeFragment.java
+++ app/src/main/java/com/atguigu/shoppingmall_1020/type/fragment/TypeFragment.java
@@ -1,12 +1,19 @@
 package com.atguigu.shoppingmall_1020.type.fragment;
 
-import android.graphics.Color;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.atguigu.shoppingmall_1020.R;
 import com.atguigu.shoppingmall_1020.base.BaseFragment;
+import com.flyco.tablayout.SegmentTabLayout;
+import com.flyco.tablayout.listener.OnTabSelectListener;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * 作者：尚硅谷-杨光福 on 2017/2/22 11:36
@@ -15,14 +22,19 @@ import com.atguigu.shoppingmall_1020.base.BaseFragment;
  * 作用：分类Fragment
  */
 public class TypeFragment extends BaseFragment {
-    private TextView textView;
+    @InjectView(R.id.tl_1)
+    SegmentTabLayout tl1;
+    @InjectView(R.id.iv_type_search)
+    ImageView ivTypeSearch;
+    @InjectView(R.id.fl_type)
+    FrameLayout flType;
+    private String[] titles = {"分类","标签"};
+
     @Override
     public View initView() {
-        textView = new TextView(mContext);
-        textView.setTextSize(20);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_type, null);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     /**
@@ -32,8 +44,29 @@ public class TypeFragment extends BaseFragment {
     @Override
     public void initData() {
         super.initData();
+        Log.e("TAG", "分类的数据被初始化了...");
+        tl1.setTabData(titles);
+        tl1.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelect(int position) {
+                Toast.makeText(mContext, "position=="+position, Toast.LENGTH_SHORT).show();
+            }
 
-        Log.e("TAG","分类的数据被初始化了...");
-        textView.setText("分类内容");
+            @Override
+            public void onTabReselect(int position) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
+    }
+
+    @OnClick(R.id.iv_type_search)
+    public void onClick() {
     }
 }
