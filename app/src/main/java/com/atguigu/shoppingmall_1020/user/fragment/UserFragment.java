29fe3af5962678860f37643e8b45142136f4122d
atguiguafu <541433511@qq.com>
2017-03-04 11:49:12 +0800

    实现滑动标题栏颜色渐变

--- app/src/main/java/com/atguigu/shoppingmall_1020/user/fragment/UserFragment.java
+++ app/src/main/java/com/atguigu/shoppingmall_1020/user/fragment/UserFragment.java
@@ -2,11 +2,19 @@ package com.atguigu.shoppingmall_1020.user.fragment;
 
 import android.graphics.Color;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.atguigu.shoppingmall_1020.R;
 import com.atguigu.shoppingmall_1020.base.BaseFragment;
+import com.hankkin.gradationscroll.GradationScrollView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * 作者：尚硅谷-杨光福 on 2017/2/22 11:36
@@ -14,15 +22,61 @@ import com.atguigu.shoppingmall_1020.base.BaseFragment;
  * QQ号：541433511
  * 作用：用户Fragment
  */
-public class UserFragment extends BaseFragment {
-    private TextView textView;
+public class UserFragment extends BaseFragment implements GradationScrollView.ScrollViewListener {
+    @InjectView(R.id.ib_user_icon_avator)
+    ImageButton ibUserIconAvator;
+    @InjectView(R.id.tv_username)
+    TextView tvUsername;
+    @InjectView(R.id.rl_header)
+    RelativeLayout rlHeader;
+    @InjectView(R.id.tv_all_order)
+    TextView tvAllOrder;
+    @InjectView(R.id.tv_user_pay)
+    TextView tvUserPay;
+    @InjectView(R.id.tv_user_receive)
+    TextView tvUserReceive;
+    @InjectView(R.id.tv_user_finish)
+    TextView tvUserFinish;
+    @InjectView(R.id.tv_user_drawback)
+    TextView tvUserDrawback;
+    @InjectView(R.id.tv_user_location)
+    TextView tvUserLocation;
+    @InjectView(R.id.tv_user_collect)
+    TextView tvUserCollect;
+    @InjectView(R.id.tv_user_coupon)
+    TextView tvUserCoupon;
+    @InjectView(R.id.tv_user_score)
+    TextView tvUserScore;
+    @InjectView(R.id.tv_user_prize)
+    TextView tvUserPrize;
+    @InjectView(R.id.tv_user_ticket)
+    TextView tvUserTicket;
+    @InjectView(R.id.tv_user_invitation)
+    TextView tvUserInvitation;
+    @InjectView(R.id.tv_user_callcenter)
+    TextView tvUserCallcenter;
+    @InjectView(R.id.tv_user_feedback)
+    TextView tvUserFeedback;
+    @InjectView(R.id.ll_root)
+    LinearLayout llRoot;
+    @InjectView(R.id.scrollview)
+    GradationScrollView scrollview;
+    @InjectView(R.id.tv_usercenter)
+    TextView tvUsercenter;
+    @InjectView(R.id.ib_user_setting)
+    ImageButton ibUserSetting;
+    @InjectView(R.id.ib_user_message)
+    ImageButton ibUserMessage;
+    /**
+     * 头部的高度
+     */
+    private int height;
+
     @Override
     public View initView() {
-        textView = new TextView(mContext);
-        textView.setTextSize(20);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_user, null);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     /**
@@ -32,7 +86,52 @@ public class UserFragment extends BaseFragment {
     @Override
     public void initData() {
         super.initData();
-        Log.e("TAG","用户的数据被初始化了...");
-        textView.setText("用户内容");
+        Log.e("TAG", "用户的数据被初始化了...");
+
+        ViewTreeObserver vto = rlHeader.getViewTreeObserver();
+        //添加监听
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+
+                //移除视图树的监听
+                rlHeader.getViewTreeObserver().removeGlobalOnLayoutListener(
+                        this);
+
+                height = rlHeader.getHeight();
+
+                scrollview.setScrollViewListener(UserFragment.this);
+            }
+        });
+
+        //设置默认是隐藏的 //设置标题的背景颜色  -透明
+        tvUsercenter.setBackgroundColor(Color.argb((int) 0, 255,0,0));
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
+    }
+
+    @Override
+    public void onScrollChanged(GradationScrollView scrollView, int x, int y, int oldx, int oldy) {
+        if (y <= 0) {
+            //设置标题的背景颜色  -透明
+            tvUsercenter.setBackgroundColor(Color.argb((int) 0, 255,0,0));
+
+        } else if (y > 0 && y <= height) { //滑动距离小于banner图的高度时，设置背景和字体颜色颜色透明度渐变
+            float scale = (float) y / height;
+            float alpha = (255 * scale);
+            //滑动距离 ： 总距离 = 改变的透明度 ： 总透明度
+            //改变的透明度 = 总透明度*(滑动距离 ：总距离)
+
+            tvUsercenter.setTextColor(Color.argb((int) alpha, 255,255,255));
+            tvUsercenter.setBackgroundColor(Color.argb((int) alpha, 255,0,0));
+        } else {
+            //滑动到banner下面设置普通颜色 - 非透明
+            tvUsercenter.setBackgroundColor(Color.argb((int) 255, 255,0,0));
+        }
     }
 }
